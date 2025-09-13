369ce0a54a2e3afcaddf5e5abc54322a9aefbc1b
Cayden <guitartok7@gmail.com>
2025-09-12 23:08:49 -0600

    Initial commit

---
+++ PersonInfo.java
@@ -0,0 +1,17 @@
+public class PersonInfo {
+    public static void main(String[] args) {
+        // Assigns the data
+        String firstName = "Maya";
+        String lastName = "Kaimana";
+        String street = "742 Coral Reef Rd";
+        String city = "Horizon Bay";
+        String zip = "96801";
+        
+        // Prints out the data
+        System.out.println("First name: " + firstName);
+        System.out.println("Last name: " + lastName);
+        System.out.println("Street: " + street);
+        System.out.println("City: " + city);
+        System.out.println("Zip: " + zip);
+    }
+}
